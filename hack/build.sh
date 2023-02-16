3d3f0547c8df9621b3e6826b007128378e71bd85
boltKrank <simon.anderson111@gmail.com>
2023-02-16 23:37:08 +1100

    Temporary GO

--- hack/build.sh
+++ hack/build.sh
@@ -5,15 +5,12 @@ set -e -x -u
 # makes builds reproducible
 export CGO_ENABLED=0
 
-go fmt ./cmd/... ./pkg/... ./test/...
-go mod vendor
-go mod tidy
+/tmp/go/bin/go fmt ./cmd/... ./pkg/... ./test/...
+/tmp/go/bin/go mod vendor
+/tmp/go/bin/go mod tidy
 
 # export GOOS=linux GOARCH=amd64
-go build -trimpath -o "imgpkg${IMGPKG_BINARY_EXT-}" ./cmd/imgpkg/...
+/tmp/go/bin/go build -trimpath -o "imgpkg${IMGPKG_BINARY_EXT-}" ./cmd/imgpkg/...
 ./imgpkg version
-
-# compile tests, but do not run them: https://github.com/golang/go/issues/15513#issuecomment-839126426
-go test --exec=echo ./...
 
 echo "Success"
