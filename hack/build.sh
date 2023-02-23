fbb657100ca52aca941361565a0ea588e0882082
boltKrank <simon.anderson111@gmail.com>
2023-02-23 11:42:01 +1100

    Removed tmp dir references

--- hack/build.sh
+++ hack/build.sh
@@ -5,12 +5,12 @@ set -e -x -u
 # makes builds reproducible
 export CGO_ENABLED=0
 
-/tmp/go/bin/go fmt ./cmd/... ./pkg/... ./test/...
-/tmp/go/bin/go mod vendor
-/tmp/go/bin/go mod tidy
+go fmt ./cmd/... ./pkg/... ./test/...
+go mod vendor
+go mod tidy
 
 # export GOOS=linux GOARCH=amd64
-/tmp/go/bin/go build -trimpath -o "imgpkg${IMGPKG_BINARY_EXT-}" ./cmd/imgpkg/...
+go build -trimpath -o "imgpkg${IMGPKG_BINARY_EXT-}" ./cmd/imgpkg/...
 ./imgpkg version
 
 echo "Success"
